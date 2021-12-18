b6e8e69599f77bd41f0b9f213b76641c6e30dbdd
mahkamehmohajerani <66775374+mahkamehmohajerani@users.noreply.github.com>
2021-12-18 15:52:33 +0330

    Update login.component.ts

--- ui/src/app/components/login/login.component.ts
+++ ui/src/app/components/login/login.component.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import{AbstractControl,FormBuilder,FormControl,FormGroup,ValidationError,Validators} from '@angular/form';
 
 @Component({
     selector: 'app-msg-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit{
+submitted=false;
+  loginForm!: FormGroup;
+
+constructor (private formBuilder:FormBuilder){
+
+}
+ngOnInit():void{
+this.loginForm = this.formBuilder.group({
+phoneNumber:[null, Validators.compose([
+    Validators.required,
+    CustomValidators.patterValidator(/[0-9]/,{hasCodeNumber : true),
+    CustomValidators.patterValidator(/[0-9]{11}$/, {hasElevenNumber:true})])],
+ password:[null,Validators.compose([
+    Validators.required,
+    CustomValidators.patterValidator(/[0-9]/,{hasNumber:true})
+});
+}
+
+get formControl():{[key:string]:AbstractControl}{
+  return this.loginForm.Controls;
+}
+onSubmit(){ console.log(this.loginForm);
+          return this.submitted=true;
+          }
+}
+export class CustomValidators{
+static patterValidator(regex:RegExp,error:ValidationErrors):ValidatorFn{
+  return(control:AbstractControl):any=>{
+    if(!control.value){
+      return null;
+    }
+    const isValid = regex.test(control.value);
+    if(isValid)
+    return null;
+    else
+    return error;
+  };
+}
