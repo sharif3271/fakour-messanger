6576bf50a619a03e63fae7fbbcaf69bb4ce30704
Sharif Eiri <sh.eiri@fakour.net>
2021-12-19 19:40:12 +0330

    enable cors for app

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,7 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+  app.enableCors();
   await app.listen(process.env.PORT);
 }
 bootstrap();
