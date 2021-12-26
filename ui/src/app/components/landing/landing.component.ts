03124f97fccf43eeac90ed2705fa0349cd594423
Reza <reza.sh.7798@gmail.com>
2021-12-26 15:28:02 +0330

    Reza Sharafi fist commir

--- ui/src/app/components/landing/landing.component.ts
+++ ui/src/app/components/landing/landing.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+
+const materialModules = [MatIconModule];
 
 @Component({
     selector: 'app-msg-landing',
     templateUrl: './landing.component.html',
-    styleUrls: ['./landing.component.scss']
-})
-export class LandinngComponent {}
+    styleUrls: ['./landing.component.scss'],
+  })
+
+export class LandinngComponent {
+  getAllUser() {}
+}
