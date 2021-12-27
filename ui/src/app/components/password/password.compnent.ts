cbf9423227d5619624f92c924c85f02655c70b82
mahkamehmohajerani <66775374+mahkamehmohajerani@users.noreply.github.com>
2021-12-27 20:43:06 +0330

    Update password.compnent.ts

--- ui/src/app/components/password/password.compnent.ts
+++ ui/src/app/components/password/password.compnent.ts
@@ -1,1 +1,10 @@
+import { component } from '@angular/core';
 
+@component({
+    selector:'app-msg-password',
+    templateUrl:'./password.component.html',
+    styleUrls:['./password.component.scss']
+})
+export class PasswordComponent{
+
+}
