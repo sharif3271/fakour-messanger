ab7f884bc60d8f8630d6138c24598dea88ea0734
Reza <reza.sh.7798@gmail.com>
2021-12-31 14:16:19 +0330

    Add chat area and some little change in left menu

--- ui/src/app/components/landing/message-area/message-area.component.ts
+++ ui/src/app/components/landing/message-area/message-area.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-area.component.scss'],
 })
 export class MessageAreaComponent implements OnInit {
-  chatActive = false;
+  chatActive = true;
 
   constructor() {}
   ngOnInit(): void {}
