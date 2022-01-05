21a1cdaf962c279d1e0b27c6bb34029a854a4f27
Reza <reza.sh.7798@gmail.com>
2022-01-05 22:43:56 +0330

    Send message in chat now working

--- ui/src/app/components/landing/message-area/message-area.component.ts
+++ ui/src/app/components/landing/message-area/message-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { IConversation, IMessage } from 'src/app/models/conversation.model';
 import { AccountService } from 'src/app/service/account.service';
 import { ChatService } from 'src/app/service/chat.service';
@@ -11,6 +11,7 @@ export class MessageAreaComponent implements OnInit {
   messageText!: string;
   chatMessages!: IMessage[];
   @Input() conversetion!: IConversation;
+  @ViewChild('chatMessageArea', { static: false }) chatMessageArea!: ElementRef;
 
   constructor(
     private chatService: ChatService,
@@ -40,5 +41,10 @@ export class MessageAreaComponent implements OnInit {
       this.chatMessages.push(newMassege);
       // this.messageText = '';
     }
+    // TODO: Add animation to scroll to last message
+    setTimeout(() => {
+      this.chatMessageArea.nativeElement.scrollTop =
+        this.chatMessageArea.nativeElement.scrollHeight;
+    }, 100);
   }
 }
